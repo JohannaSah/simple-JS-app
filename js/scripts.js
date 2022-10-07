8af6c367dafee4eaed55602ba15b7a0026e73f37
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2022-10-07 17:18:44 +0200

    created pokemonList variable
    contains 4 pokemon with 4 properties each

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
+let pokemonList = [
+  {name: "Bulbasaur", types: ['grass', 'poison'], height: 0.7, weight: 6.9},
+  {name: "Butterfree", types: ['bug', 'flying'], height: 1.1, weight: 32},
+  {name: "Starmie", types: ['psychic', 'water'], height: 1.1, weight: 80},
+  {name: "Ducklett", types: ['water', 'flying'], height: 0.5, weight: 5.5}
 
-alert('Hello World');
-
-let favoriteFood = 'Dim Sum';
-document.write(favoriteFood);
+]

f5d9a636be8794ca281a59220ba95da34d3748b6
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2022-10-07 22:20:17 +0200

    changed format of pokemonList and added for loop
    for loop contains if-else conditionals for the weight property of the pokemons in the pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,45 @@ this variable is in the form of an array
  and numbers
  */
 let pokemonList = [
-  {name: "Bulbasaur", types: ['grass', 'poison'], height: 0.7, weight: 6.9},
-  {name: "Butterfree", types: ['bug', 'flying'], height: 1.1, weight: 32},
-  {name: "Starmie", types: ['psychic', 'water'], height: 1.1, weight: 80},
-  {name: "Ducklett", types: ['water', 'flying'], height: 0.5, weight: 5.5}
+  {
+    name: "Bulbasaur",
+    types: ['grass', 'poison'],
+    height: 0.7,
+    weight: 6.9
+  },
+  {
+    name: "Butterfree",
+    types: ['bug', 'flying'],
+    height: 1.1,
+    weight: 32
+  },
+  {
+    name: "Starmie",
+    types: ['psychic', 'water'],
+    height: 1.1,
+    weight: 80
+  },
+  {
+    name: "Ducklett",
+    types: ['water', 'flying'],
+    height: 0.5,
+    weight: 5.5
+  }
+];
 
-]
+/*
+the following is a for loop that contains if-else conditionals
+chose the weight as the property for the conditionals, as there was a larger spread
+added more than required to make the sentences nicer
+*/
+for ( let i = 0; i < pokemonList.length; i++){
+  if (pokemonList[i].weight < 50 && pokemonList[i].weight > 10) {
+    document.write(pokemonList[i].name + " is an average sized pokemon " + "(" + pokemonList[i].weight + "kg)" + ". ");
+  }
+  else if (pokemonList[i].weight < 10) {
+    document.write(pokemonList[i].name + " is a small pokemon " + "(" + pokemonList[i].weight + "kg)" + ". ");
+  }
+  else {
+    document.write("Wow, "+ pokemonList[i].name + " is a really large pokemon " + "(" + pokemonList[i].weight + "kg)" + ". ");
+  }
+}

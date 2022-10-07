24ec20a49e95971573593bde82807d537628238c
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2022-10-07 17:24:58 +0200

    commented on the variable
    describes the variable and its contents in detail

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+
+/*
+this variable is in the form of an array
+ the array is made up of 4 objects
+ each object has 4 properties
+ the properties have values in the form of string, an array of string values
+ and numbers
+ */
 let pokemonList = [
   {name: "Bulbasaur", types: ['grass', 'poison'], height: 0.7, weight: 6.9},
   {name: "Butterfree", types: ['bug', 'flying'], height: 1.1, weight: 32},
